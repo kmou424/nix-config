dd6393c155a30edc5567a5a22aa342944a7457c7
kmou424 <me@kmou424.moe>
2024-01-15 21:24:06 +0800

    chore: install some pkgs for cinnamon

--- modules/desktop/cinnamon.nix
+++ modules/desktop/cinnamon.nix
@@ -1,12 +1,18 @@
 { pkgs, ... }: {
-	services.xserver = {
-		enable = true;
-                # Enable touchpad support (enabled default in most desktopManager).
-                libinput.enable = true;
-		displayManager.lightdm.enable = true;
-		desktopManager = {
-			cinnamon.enable = true;
-		};
-		displayManager.defaultSession = "cinnamon";
-	};
+  services.xserver = {
+    enable = true;
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput.enable = true;
+    displayManager.lightdm.enable = true;
+    desktopManager = {
+      cinnamon.enable = true;
+    };
+    displayManager.defaultSession = "cinnamon";
+  };
+
+  environment.systemPackages = with pkgs; [
+    gnome.gnome-system-monitor
+    gnome.gpaste
+    coreutils-prefixed
+  ];
 }
